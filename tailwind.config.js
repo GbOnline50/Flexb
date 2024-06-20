22f618b5327c4cc7a00e995cd6397792003a8d13
Allan Cristian Krause <allan.krause09@hotmail.com>
2024-06-20 19:34:47 -0400

    servico 20-06

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,30 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    textColor: theme => theme('colors'),
-    textColor: {
-      'primary': '#ffffff',
-      'secondary': '#ffffff',
-      'danger': '#ffffff',
-    },
     fontFamily: {
       'sans': ['Nunito', 'Helvetica', 'Arial', 'sans-serif']
     },
-    colors:{
-      'black': '#222222',
-      'white': '#ffffff'
-    },
     extend: {
+      colors:{
+        black: '#222222',
+        white: '#ffffff',
+        cinza: {
+          letra: '#7c7c7c',
+          fundo: '#f7f7f7'
+        },
+        blue: {
+          forte: '#122ab2',
+          fraco: '#2c70ff'
+        },
+        pink: '#f9265e',
+        roxo: '#ae81ff',
+        azul: '#66d9eb',
     },
+    flex:{
+      '2' : '1 1 160px',
+      '300' : '2 1 300px',
+    },
+  }
   },
   plugins: [],
 }
-
